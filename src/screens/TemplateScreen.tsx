a0d169e034419774c0da4c20aa407179c42041f8
jodydream <270721697@qq.com>
2024-10-18 13:46:05 +1300

    修改模版页

--- src/screens/TemplateScreen.tsx
+++ src/screens/TemplateScreen.tsx
@@ -11,32 +11,50 @@ import {
 } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import styles from '../styles/TemplateScreen.styles';
+import globalStyles from '../styles/globalStyles';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParamList} from '../navigation/AppNavigator'; // 导入导航类型
+import theme from '../styles/theme/theme'; // 自定义主题
+type Props = StackScreenProps<RootStackParamList, 'TestScreen'>;
 
-const LoginScreen = () => {
-
+const TemplateScreen = ({navigation, route}: Props) => {
+  const goBack = () => {
+    navigation.goBack();
+  };
   return (
     <View style={styles.wholeContaine}>
-      <View style={styles.mainContainer}>
+      {/* part1 */}
+      <View style={styles.top_container}>
+        {/* 1 状态栏 */}
         <StatusBar
-          barStyle="dark-content"
-          backgroundColor="transparent"
-          translucent
+          barStyle="light-content"
+          backgroundColor={theme.colors.primary}
+          translucent={true} // 如果不希望内容重叠在状态栏下，关闭透明??
         />
 
-        {/* 标题和 Logo 区域 */}
-        <View style={styles.titleContainer}>
+        {/* 2 导航栏 */}
+        <View style={styles.nav_container}>
           <View style={styles.logoContainer}>
-            <Image
-              source={require('../../assets/images/jadcup_logo.png')}
-              style={styles.logo}
-            />
+            <TouchableOpacity onPress={goBack}>
+              <AntDesign name="left" size={20} color="white" />
+            </TouchableOpacity>
           </View>
+
+          <Text style={styles.nav_title_text}>入仓库</Text>
+
+          <TouchableOpacity onPress={goBack}>
+            <AntDesign name="setting" size={20} color="white" />
+          </TouchableOpacity>
         </View>
-
-
       </View>
+
+      {/* ---------分割线------- */}
+      <View style={globalStyles.line_view_tiny}></View>
+
+      {/* part 2 */}
+      <View style={styles.mainContainer}></View>
     </View>
   );
 };
 
-export default LoginScreen;
+export default TemplateScreen;
--- src/styles/TemplateScreen.styles.ts
+++ src/styles/TemplateScreen.styles.ts
@@ -1,6 +1,6 @@
 // src/screens/MainScreen.styles.ts
 import { StyleSheet } from 'react-native';
-import { LAYOUT, SPACING } from '../styles/theme/dimensions'; // 尺寸常量
+import { LAYOUT, SPACING,FONT_SIZE } from '../styles/theme/dimensions'; // 尺寸常量
 import theme from '../styles/theme/theme'; // 自定义主题 
 
 const styles = StyleSheet.create({
@@ -11,10 +11,9 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   // 2级别
-  navContainer: {
+  top_container: {
     marginTop: LAYOUT.statusBarHeight,
-    marginHorizontal: LAYOUT.margingHorizontal,
-    // backgroundColor:'red'
+    backgroundColor:theme.colors.primary
   },
   mainContainer: {
     flex: 1, //沾满剩下的区域
@@ -26,9 +25,12 @@ const styles = StyleSheet.create({
   //=================
   // 3级
   nav_container: {
+    marginHorizontal: LAYOUT.margingHorizontal,
     flexDirection: 'row',
     alignItems: 'center',
-    height: 44,
+    height: LAYOUT.nav_height,
+    justifyContent:'space-between'
+    
   },
   logoContainer: {
     justifyContent: 'center',
@@ -39,7 +41,12 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     resizeMode: 'contain',
-  }
+  },
+  nav_title_text: {
+    fontSize: FONT_SIZE.BIG1,
+    fontWeight: 'bold',
+    color:'white',
+  },
 });
 
 export default styles;
